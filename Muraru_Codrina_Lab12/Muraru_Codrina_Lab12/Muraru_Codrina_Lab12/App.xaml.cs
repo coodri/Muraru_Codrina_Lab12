d5c92a80e4bef5d73550a9a12f2869b1dd08e9ec
Codrina Muraru <codrina.muraru@yahoo.com>
2022-01-16 14:51:03 +0200

    Add project files.

---
+++ Muraru_Codrina_Lab12/Muraru_Codrina_Lab12/Muraru_Codrina_Lab12/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Muraru_Codrina_Lab12.Data;
+
+namespace Muraru_Codrina_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
